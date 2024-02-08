677f1bbfdcbac79a06b9279fb7fb9f6801116826
Jimmy.|\| <jimmy09140456@gmail.com>
2024-02-08 17:19:01 +0000

    Added the script to create new branches and tags for new releases from upstream.

---
+++ build.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+echo "The build script to create branches and tags for new release from upstream."
+echo "The first argument is the upstream tag name."
+echo "Second optional argument is the build number, e.g. b1"
+echo "Usage: ./build.sh v5.4.0"
+
+#INPUT VARIABLES
+uptag="$1"
+[ "$2" ] && bn="$2" || bn="b1"
+branch_name="build-${uptag:}"
+tag_name="$uptag-$bn"
+
+#CONFIGURATION VARIABLES
+build_branch="main-build"
+
+echo "Checkout the tag from upstream"
+git checkout tags/$uptag
+
+echo "Create a new brach for editing"
+git checkout -b $branch_name
+
+echo "Copy the Github actions from build branch"
+for i in build-bin nightly-build
+do
+    action_yml=".github/workflows/$i.yml"
+    git show $build_branch:$action_yml
+    echo "Copied $action_yml"
+done
+
+echo "Commit the changes"
+git commit -am "Added the build actions"
+
+echo "Push to Github"
+git push origin $branch_name
+
+echo "Create a tag for release and push to Github"
+git tag $tag_name
+git push --tags
+
+echo "Done. Please check the Github action results on the website"
